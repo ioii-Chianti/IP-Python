f9ed1d311a0a9a6931dbd5232f23328956fcae18
Chianti Sera <ioii.chianti@gmail.com>
2021-06-11 05:19:05 +0800

    需要優化判斷鄰居

--- PyChallenge.py
+++ PyChallenge.py
@@ -2,76 +2,145 @@ from random import *
 
 class Grid:
     def __init__(self):
-        self.grid = [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ]
-        self.Max = 2
+        self.board = [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ]
+        self.newScore = 0
+        self.update = False
         self.randomNew()
         self.randomNew()
 
     def randomNew(self):
+        def isValid(r, c):
+            if self.board[r][c]:
+                return False
+            return True
+
         while True:
-            x, y = randint(0, 3), randint(0, 3)
-            if self.isValid(x, y):
-                self.grid[x][y] = 2
+            r, c = randint(0, 3), randint(0, 3)
+            if isValid(r, c):
+                self.board[r][c] = 2
                 break
     
-    def isValid(self, x, y):
-        if self.grid[x][y]:
-            return False
-        return True
-
     def rotate(self):
-        print('Rot')
+        self.board = list(map(list, zip(*self.board)))[::-1]
 
     def collapse(self):
-        print('colla')
+        Fix, self.newScore, self.update = [-1, -1, -1, -1], 0, False
+        def merge(r, c):
+            if self.board[r][c]:
+                if self.board[r][c-1] == 0:
+                    self.board[r][c-1] = self.board[r][c]
+                    if c > 1:
+                        merge(r, c - 1)
+                    self.board[r][c] = 0
+                    self.update = True
+                elif self.board[r][c-1] == self.board[r][c] and Fix[r] < c - 1:
+                    self.board[r][c-1] += self.board[r][c]
+                    self.newScore += self.board[r][c-1]
+                    self.board[r][c] = 0
+                    Fix[r] = c - 1
+                    self.update = True
 
+        for row in range(4):
+            for col in range(1, 4):
+                merge(row, col)
+        return self.update
+        
 
 class Game:
     def __init__(self, target):
         self.Score = 0
         self.Move = 0
         self.Target = target
-        self.Board = Grid()
+        self.GridObj = Grid()
         self.Result = ''
 
-    @property
-    def Score(self):
-        return self.Score
-    @property
-    def Move(self):
-        return self.Move
-
     def Swipe(self, direction):
-        while direction:
-            self.Board.rotate()
-            direction -= 1
-        self.Board.collapse()
+        times = {'W': 1, 'S': 3, 'A': 0, 'D': 2}
+        time = times[direction]
+        while time:
+            self.GridObj.rotate()
+            time -= 1
+        needUpdate = self.GridObj.collapse()
+
+        time = 4 - times[direction]
+        while time:
+            self.GridObj.rotate()
+            time -= 1
+
+        if needUpdate:
+            self.GridObj.randomNew()
+            self.Move += 1
+            self.Score += self.GridObj.newScore
+        print('shifted, ', end='') if needUpdate else print('not shifted, ', end='')
+        print(f'{self.GridObj.newScore} points')
     
     def View(self):
-        for row in self.Board:
+        # 改成呼叫這個物件就 return 容器
+        for row in self.GridObj.board:
             for val in row:
-                print(f'{val} ')
+                print(f'{val}', end = ' ')
             print()
     
     def Finished(self):
-        if self.Board.Max == self.Target:
+        if self.GridObj.newScore >= self.Target:
             self.Result = 'You Win!'
             return True
 
-        for row in self.Board:
-            for val in row:
+        GoOn = False
+        for row in range(4):
+            for col in range(4):
+                val = self.GridObj.board[row][col]
+                # still have 0 on the board
                 if not val:
                     return False
+
+                # Check all neighbors
+                if (row, col) == (0, 0):
+                    GoOn = val == self.GridObj.board[row + 1][col] or \
+                           val == self.GridObj.board[row][col + 1]
+                elif (row, col) == (0, 3):
+                    GoOn = val == self.GridObj.board[row + 1][col] or \
+                           val == self.GridObj.board[row][col - 1]
+                elif (row, col) == (3, 0):
+                    GoOn = val == self.GridObj.board[row - 1][col] or \
+                           val == self.GridObj.board[row][col + 1]
+                elif (row, col) == (3, 3):
+                    GoOn = val == self.GridObj.board[row - 1][col] or \
+                           val == self.GridObj.board[row][col - 1]
+                elif row == 0:
+                    GoOn = val == self.GridObj.board[row + 1][col] or \
+                           val == self.GridObj.board[row][col - 1] or \
+                           val == self.GridObj.board[row][col + 1]
+                elif col == 0:
+                    GoOn = val == self.GridObj.board[row - 1][col] or \
+                           val == self.GridObj.board[row + 1][col] or \
+                           val == self.GridObj.board[row][col + 1]
+                elif row == 3:
+                    GoOn = val == self.GridObj.board[row - 1][col] or \
+                           val == self.GridObj.board[row][col - 1] or \
+                           val == self.GridObj.board[row][col + 1]
+                elif col == 3:
+                    GoOn = val == self.GridObj.board[row - 1][col] or \
+                           val == self.GridObj.board[row + 1][col] or \
+                           val == self.GridObj.board[row][col - 1]
+                else:
+                    GoOn = val == self.GridObj.board[row - 1][col] or \
+                           val == self.GridObj.board[row + 1][col] or \
+                           val == self.GridObj.board[row][col - 1] or \
+                           val == self.GridObj.board[row][col + 1]
+        if (GoOn):
+            return False
         self.Result = 'You Lose!'
         return True
 
-
-target = input("Enter your target value: ")
+target = int(input("Enter your target value: "))
 Game2048 = Game(target)
 
 while True:
+    print()
     Game2048.View()
-    
-    
+    action = input('Up, Down, Left, Right: ')
+    Game2048.Swipe(action)
     if Game2048.Finished():
         print(Game2048.Result)
+        break
